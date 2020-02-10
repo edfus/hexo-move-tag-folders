43c74a68759a166ee4b2c26bf563986f7979070c
edfus <edfusedfus@gmail.com>
2020-02-10 19:31:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,109 @@
 'use strict';
 const fs = require('hexo-fs');
+var cheerio = require('cheerio');
+var categoriesInfo = [];
+var postLinks = [];
+var indexLinks = [];
+
+hexo.extend.filter.register('after_post_render', function(data){
+if(data.published){
+  if(data.layout === 'post')
+    postLinks.push(data.permalink.substring(data.permalink.length, data.permalink.lastIndexOf('.')+4)+'index.html');
+  else if(data.layout.indexOf('index') !== -1 )
+    indexLinks.push(data.permalink.substring(data.permalink.length, data.permalink.lastIndexOf('.')+4)+'index.html');
+}
+})
 
 
 hexo.extend.filter.register('before_exit', function(data){
   var moveTags = function (exists) {
     if(exists==false)
       return '';
-    else
+    else{
       categorysTagsAdd();
+   }
   };
 
   fs.exists(`public/${hexo.config.tag_dir}`, moveTags);
 },11);
 
-function categorysTagsAdd(){
-  fs.readFile(hexo.database.options.path, [], function(err,data){  if(err)  console.log("读取文件fail " + err);  else {
-    var result = JSON.parse(data);
-    var categoriesInfo = [];
+function indexPagesNum(path,num){
+  var exist;
+  (function(){  fs.exists(`public${path}/page/${num}`, function(exists){
+    exist = exists;
+  })})();
 
+  if(!exist)
+     return num-1;
+  else
+     return indexPagesNum(path,num+1);
+}
+
+function tagHrefChange(){
+  var pageLink = [];
+
+  categoriesInfo.forEach(function(category){
+      pageLink.push(`${hexo.config.category_dir}/${category.name}/index.html`);
+      category.tags.forEach(function(tag){
+        pageLink.push(`${hexo.config.category_dir}/${category.name}/${tag.name}/index.html`);
+        pageLink.push(`${hexo.config.tag_dir}/${tag.name}/index.html`);
+      })
+  })
+
+  pageLink.push('index.html');
+
+  for(var i = indexPagesNum('',2); i > 1; i-- ){
+    pageLink.push(`public/page/${i}/index.html`);
+  }
+
+  pageLink.push(`${hexo.config.more_path}/index.html`);
+
+  for(var i = indexPagesNum(hexo.config.more_path,2); i > 1; i-- ){
+    pageLink.push(`public${hexo.config.more_path}/page/${i}/index.html`);
+  }
+
+  var links = postLinks.concat(indexLinks).concat(pageLink);
+
+  links.forEach(function(link){
+    fs.readFile('public/'+link, [], function(err,data){  if(err){  //console.log("读取文件fail " + err);
+                                                                                                        return'';} else {
+        var $ = cheerio.load(data, {
+          ignoreWhitespace: false,
+          xmlMode: false,
+          lowerCaseTags: false,
+          decodeEntities: false
+        });
+
+        $('a[rel=tag]').each(function(){
+          if ($(this).attr('href')){
+            var href = $(this).attr('href').substring($(this).attr('href').length -1, 0);
+            let tagName = href.substring(href.length, href.lastIndexOf('/')+1);
+            categoriesInfo.some(function(category){
+              for(var i = 0; i < category.tags.length; i++ ){
+                // console.log(`${encodeURI(category.tags[i].name)} && ${tagName}`);
+                if(encodeURI(category.tags[i].name) == tagName){
+                  href = `/${hexo.config.category_dir}/${category.name}/${tagName}`;
+                  return true;
+                }
+              }
+            })
+            if(href !== $(this).attr('href').substring($(this).attr('href').length -1, 0))
+                $(this).attr('href', `${href}`);
+            else;// console.log(`irregular href format: ${href},${$(this).attr('href')}`);
+          }else{
+            console.info&&console.info("no href attr, skipped...");
+            // console.info&&console.info($(this));
+          }
+        });
+        data = $.html();
+    fs.writeFile('public/'+link,data, [], function(err){  if(err)  console.log("写入文件fail " + err);  else ;})
+    }})
+  })
+}
+
+async function categorysTagsAdd(){
+  await fs.readFile(hexo.database.options.path, [], function(err,data){  if(err)  console.log("读取文件fail " + err);  else {
+    var result = JSON.parse(data);
       result.models.Category.forEach( function(Category){
         var category ={
           name: Category.name,
@@ -58,6 +144,7 @@ function categorysTagsAdd(){
           category.tags = deteleRepeatedObject(category.tags);
           console.log(operateFolders(category));
       });
+      tagHrefChange();
   }});
   
   
